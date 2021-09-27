8f9803fecd929dc52edea40259a2ab55b4720c3c
ASingleton <asingleton@creativeone.com>
2021-09-27 13:14:31 -0500

    Changed path on DrawSVGPlugin

--- demo-1/pages/tenYears.js
+++ demo-1/pages/tenYears.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 import gsap from 'gsap'
-import {DrawSVGPlugin} from './../Components/DrawSVGPlugin.js'
+import {DrawSVGPlugin} from '../components/DrawSVGPlugin.js'
 const YagerLogo = () => {
     useEffect(() => {
       gsap.registerPlugin(DrawSVGPlugin);
